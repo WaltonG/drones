1af2785556accc847d56a7671f393a3ec4918d6e
WaltonG <waltongoga@gmail.com>
2022-01-13 07:38:36 +0300

    Preloding required data to database

--- src/main/java/com/musala/drones/DronesApplication.java
+++ src/main/java/com/musala/drones/DronesApplication.java
@@ -1,5 +1,8 @@
 package com.musala.drones;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,6 +10,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.task.TaskExecutor;
 
+import com.musala.drones.models.Drone;
+import com.musala.drones.models.Medication;
+import com.musala.drones.models.ModelType;
+import com.musala.drones.models.Status;
+import com.musala.drones.repositories.DroneRepository;
 import com.musala.drones.services.BatteryLevelAuditService;
 import com.musala.drones.services.DroneService;
 import com.musala.drones.util.BatteryLevelTask;
@@ -30,4 +38,24 @@ public class DronesApplication {
 		final BatteryLevelTask task = new BatteryLevelTask(droneService, auditService);
 		return args -> executor.execute(task.startTaskTimer());
 	}
+	
+	 @Bean
+	    CommandLineRunner init(DroneRepository repository) {
+	        return args -> {
+
+				List<Medication> droneMedications = Arrays.asList(new Medication("Zitromax", 30, "ZITROMAX_500"),
+				    new Medication("Lipitor", 30.7, "LIPTOR_100"), new Medication("Paracetamol", 10.5, "PARACETAMOL_500")
+	            );
+
+				Drone drone = new Drone();
+				drone.setBatteryPercentage(100);
+				drone.setWeightLimit(100);
+				drone.setModel(ModelType.Cruiserweight);
+				drone.setState(Status.IDLE);
+				droneMedications.forEach(drone::addMedication);
+				drone.setSerialNumber("cruiserWeight1");
+				repository.save(drone);
+
+			};
+	    }
 }
